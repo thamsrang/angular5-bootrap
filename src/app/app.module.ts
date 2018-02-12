6425c30d19603aa6e5c4c1cb1504cc5ba1bd6db8
ThamsRang <thamsrang@gmail.com>
2018-02-12 12:28:06 +0530

    Component Creation for Login, Header, Footer and Dashboard Side Menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { LoginComponent } from './login/login.component';
+import { DashSidemenuComponent } from './dash-sidemenu/dash-sidemenu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppFooterComponent,
+    LoginComponent,
+    DashSidemenuComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppHeaderComponent, AppComponent, AppFooterComponent]
 })
 export class AppModule { }
