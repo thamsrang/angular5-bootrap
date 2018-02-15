a60b1fc4a33bc14ea28b1fd9dfbde7cf084a9ed7
thamsrang@gmail.com <thamsrang@gmail.com>
2018-02-15 19:28:49 +0530

    Moved npm lib files to assest. Added Font Awesome

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'sso/logout', redirectTo: '', pathMatch: 'full' }
 ];
 
 
